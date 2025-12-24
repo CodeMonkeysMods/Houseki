a6be3e2ea1bbc14d083bdd81aa63ba9f5543b119
NanamiPizza <chrispizza181@gmail.com>
2025-12-23 20:28:19 -0500

    Fixed Spelling error that broke helmet trims

--- src/main/java/anya/pizza/pizzasoat/datagen/ModItemTagProvider.java
+++ src/main/java/anya/pizza/pizzasoat/datagen/ModItemTagProvider.java
@@ -4,10 +4,15 @@ import anya.pizza.pizzasoat.item.ModItems;
 import anya.pizza.pizzasoat.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.data.tag.ValueLookupTagProvider;
 import net.minecraft.item.Items;
+import net.minecraft.item.equipment.trim.ArmorTrimMaterial;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagKey;
 
+import java.util.Comparator;
 import java.util.concurrent.CompletableFuture;
 
 public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
