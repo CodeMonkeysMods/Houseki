a42171d65284bc6f53decd0140c0cb00d8c1b62d
NanamiPizza <chrispizza181@gmail.com>
2025-12-23 21:09:10 -0500

    Added custom slots for custom smithing templates.

--- src/main/java/anya/pizza/pizzasoat/item/custom/PinkuTemplateItem.java
+++ src/main/java/anya/pizza/pizzasoat/item/custom/PinkuTemplateItem.java
@@ -18,35 +18,25 @@ public class PinkuTemplateItem extends SmithingTemplateItem {
     private static final Formatting DESCRIPTION_FORMATTING = Formatting.LIGHT_PURPLE;
     private static final Formatting TITLE_FORMATTING = Formatting.DARK_PURPLE;
     private static final Text PINKU_UPGRADE_TEXT = Text.translatable(Util.createTranslationKey("upgrade", Identifier.of(PizzasOAT.MOD_ID,"pinku_upgrade"))).formatted(TITLE_FORMATTING);
-    private static final Text PINKU_UPGRADE_APPLIES_TO_TEXT = Text.translatable(
-                    Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID, "smithing_template.pinku_upgrade.applies_to"))
-            )
-            .formatted(DESCRIPTION_FORMATTING);
-    private static final Text PINKU_UPGRADE_INGREDIENTS_TEXT = Text.translatable(
-                    Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID,"smithing_template.pinku_upgrade.ingredients"))
-            )
-            .formatted(DESCRIPTION_FORMATTING);
-    private static final Text PINKU_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Text.translatable(
-            Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID,"smithing_template.pinku_upgrade.base_slot_description"))
-    );
-    private static final Text PINKU_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Text.translatable(
-            Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID,"smithing_template.pinku_upgrade.additions_slot_description"))
-    );
+    private static final Text PINKU_UPGRADE_APPLIES_TO_TEXT = Text.translatable(Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID, "smithing_template.pinku_upgrade.applies_to"))).formatted(DESCRIPTION_FORMATTING);
+    private static final Text PINKU_UPGRADE_INGREDIENTS_TEXT = Text.translatable(Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID,"smithing_template.pinku_upgrade.ingredients"))).formatted(DESCRIPTION_FORMATTING);
+    private static final Text PINKU_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Text.translatable(Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID,"smithing_template.pinku_upgrade.base_slot_description")));
+    private static final Text PINKU_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Text.translatable(Util.createTranslationKey("item", Identifier.of(PizzasOAT.MOD_ID,"smithing_template.pinku_upgrade.additions_slot_description")));
 
     public PinkuTemplateItem(Text appliesToText, Text ingredientsText, Text baseSlotDescriptionText, Text additionsSlotDescriptionText, List<Identifier> emptyBaseSlotTextures, List<Identifier> emptyAdditionsSlotTextures, Settings settings) {
         super(appliesToText, ingredientsText, baseSlotDescriptionText, additionsSlotDescriptionText, emptyBaseSlotTextures, emptyAdditionsSlotTextures, settings);
     }
 
-    private static final Identifier EMPTY_ARMOR_SLOT_HELMET_TEXTURE = Identifier.ofVanilla("item/empty_armor_slot_helmet");
-    private static final Identifier EMPTY_ARMOR_SLOT_CHESTPLATE_TEXTURE = Identifier.ofVanilla("item/empty_armor_slot_chestplate");
-    private static final Identifier EMPTY_ARMOR_SLOT_LEGGINGS_TEXTURE = Identifier.ofVanilla("item/empty_armor_slot_leggings");
-    private static final Identifier EMPTY_ARMOR_SLOT_BOOTS_TEXTURE = Identifier.ofVanilla("item/empty_armor_slot_boots");
-    private static final Identifier EMPTY_SLOT_HOE_TEXTURE = Identifier.ofVanilla("item/empty_slot_hoe");
-    private static final Identifier EMPTY_SLOT_AXE_TEXTURE = Identifier.ofVanilla("item/empty_slot_axe");
-    private static final Identifier EMPTY_SLOT_SWORD_TEXTURE = Identifier.ofVanilla("item/empty_slot_sword");
-    private static final Identifier EMPTY_SLOT_SHOVEL_TEXTURE = Identifier.ofVanilla("item/empty_slot_shovel");
-    private static final Identifier EMPTY_SLOT_PICKAXE_TEXTURE = Identifier.ofVanilla("item/empty_slot_pickaxe");
-    private static final Identifier EMPTY_SLOT_INGOT_TEXTURE = Identifier.ofVanilla("item/empty_slot_ingot");
+    private static final Identifier EMPTY_ARMOR_SLOT_HELMET_TEXTURE = Identifier.ofVanilla("container/slot/helmet");
+    private static final Identifier EMPTY_ARMOR_SLOT_CHESTPLATE_TEXTURE = Identifier.ofVanilla("container/slot/chestplate");
+    private static final Identifier EMPTY_ARMOR_SLOT_LEGGINGS_TEXTURE = Identifier.ofVanilla("container/slot/leggings");
+    private static final Identifier EMPTY_ARMOR_SLOT_BOOTS_TEXTURE = Identifier.ofVanilla("container/slot/boots");
+    private static final Identifier EMPTY_SLOT_HOE_TEXTURE = Identifier.ofVanilla("container/slot/hoe");
+    private static final Identifier EMPTY_SLOT_AXE_TEXTURE = Identifier.ofVanilla("container/slot/axe");
+    private static final Identifier EMPTY_SLOT_SWORD_TEXTURE = Identifier.ofVanilla("container/slot/sword");
+    private static final Identifier EMPTY_SLOT_SHOVEL_TEXTURE = Identifier.ofVanilla("container/slot/shovel");
+    private static final Identifier EMPTY_SLOT_PICKAXE_TEXTURE = Identifier.ofVanilla("container/slot/pickaxe");
+    private static final Identifier EMPTY_SLOT_PINKU_TEXTURE = Identifier.of(PizzasOAT.MOD_ID, "container/slot/pinku_slot");
 
     public static SmithingTemplateItem createPinkuUpgrade(Settings settings) {
         return new SmithingTemplateItem(
@@ -54,8 +44,8 @@ public class PinkuTemplateItem extends SmithingTemplateItem {
                 PINKU_UPGRADE_INGREDIENTS_TEXT,
                 PINKU_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT,
                 PINKU_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT,
-                getNetheriteUpgradeEmptyBaseSlotTextures(),
-                getNetheriteUpgradeEmptyAdditionsSlotTextures(),
+                getPinkuUpgradeEmptyBaseSlotTextures(),
+                getPinkuUpgradeEmptyAdditionsSlotTextures(),
                 settings
         );
     }
@@ -69,7 +59,7 @@ public class PinkuTemplateItem extends SmithingTemplateItem {
         textConsumer.accept(PINKU_UPGRADE_INGREDIENTS_TEXT);
     }
 
-    private static List<Identifier> getNetheriteUpgradeEmptyBaseSlotTextures() {
+    private static List<Identifier> getPinkuUpgradeEmptyBaseSlotTextures() {
         return List.of(
                 EMPTY_ARMOR_SLOT_HELMET_TEXTURE,
                 EMPTY_SLOT_SWORD_TEXTURE,
@@ -83,7 +73,7 @@ public class PinkuTemplateItem extends SmithingTemplateItem {
         );
     }
 
-    private static List<Identifier> getNetheriteUpgradeEmptyAdditionsSlotTextures() {
-        return List.of(EMPTY_SLOT_INGOT_TEXTURE);
+    private static List<Identifier> getPinkuUpgradeEmptyAdditionsSlotTextures() {
+        return List.of(EMPTY_SLOT_PINKU_TEXTURE);
     }
 }
